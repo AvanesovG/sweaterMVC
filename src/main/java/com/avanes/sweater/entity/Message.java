5d23f464eb1577c2eb7a1bd355e09c56599957be
AvanesovG <grigoriiavanesov@gmail.com>
2020-11-07 20:33:26 +0300

    add form add Message

--- src/main/java/com/avanes/sweater/entity/Message.java
+++ src/main/java/com/avanes/sweater/entity/Message.java
@@ -5,49 +5,45 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+
 @Entity
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private String text;
-    private String teg;
+    private Integer id;
 
+    private String text;
+    private String tag;
 
     public Message() {
-
     }
 
-    public Message(Long id) {
-        this.id = id;
-    }
-
-    public Message(String text, String teg) {
+    public Message(String text, String tag) {
         this.text = text;
-        this.teg = teg;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getText() {
-        return text;
+        this.tag = tag;
     }
 
     public void setText(String text) {
         this.text = text;
     }
 
-    public String getTeg() {
-        return teg;
+    public String getText() {
+        return text;
     }
 
-    public void setTeg(String teg) {
-        this.teg = teg;
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
     }
 }
