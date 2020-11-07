ca4d4799872c2164a0291e18bfe4e7cc88488b6f
grigoriiavanesov@gmail.com <Grisha2401>
2020-11-07 18:11:37 +0300

    Add class Message

---
+++ src/main/java/com/avanes/sweater/entity/Message.java
@@ -0,0 +1,53 @@
+package com.avanes.sweater.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String text;
+    private String teg;
+
+
+    public Message() {
+
+    }
+
+    public Message(Long id) {
+        this.id = id;
+    }
+
+    public Message(String text, String teg) {
+        this.text = text;
+        this.teg = teg;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getTeg() {
+        return teg;
+    }
+
+    public void setTeg(String teg) {
+        this.teg = teg;
+    }
+}
