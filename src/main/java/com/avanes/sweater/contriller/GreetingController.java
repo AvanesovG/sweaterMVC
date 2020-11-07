ef825131f1ee55ed7ad354202a12e06db292821b
grigoriiavanesov@gmail.com <Grisha2401>
2020-11-07 18:02:32 +0300

    Add class GreetingController

--- src/main/java/com/avanes/sweater/contriller/GreetingController.java
+++ src/main/java/com/avanes/sweater/contriller/GreetingController.java
@@ -1,7 +1,6 @@
 package com.avanes.sweater.contriller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -10,11 +9,19 @@ import java.util.Map;
 @Controller
 public class GreetingController {
 
+
+    @GetMapping
+    public String main(Map<String, Object> madel) {
+        madel.put("some", "Все ок!");
+        return "main";
+    }
+
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World")
                                    String name, Map<String, Object> model) {
         model.put("name", name);
         return "greeting";
     }
+
 
 }
